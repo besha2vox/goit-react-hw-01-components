35630b5736c0625a1b36db31695078fecd940127
Serhii Revva <107750742+besha2vox@users.noreply.github.com>
2022-12-08 18:53:32 +0200

    bugfix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -20,7 +20,7 @@ export default function App() {
                 />
                 <Statistics title="Upload stats" stats={data} />
                 <FriendList friends={friends} />
-                <TransactionHistory items={transactions} />;
+                <TransactionHistory items={transactions} />
             </Wrapper>
         </Container>
     );
