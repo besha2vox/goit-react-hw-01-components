171f59cf7f0d6c149fb458cb235137b5bb433755
Serhii Revva <besha2vox@gmail.com>
2023-01-15 23:12:25 +0200

    use templete from technical task

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -3,22 +3,29 @@ import { FriendListItem } from 'components/FriendListItem/FriendListItem';
 import PropTypes from 'prop-types';
 
 function FriendList({ friends }) {
-    return (
-        <FriendUnnumberedList data-action="id-3">
-            {friends.map(FriendListItem)}
-        </FriendUnnumberedList>
-    );
+  return (
+    <FriendUnnumberedList data-action="id-3">
+      {friends.map(({ id, avatar, name, isOnline }) => (
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
+    </FriendUnnumberedList>
+  );
 }
 
 FriendList.propTypes = {
-    friends: PropTypes.arrayOf(
-        PropTypes.shape({
-            avatar: PropTypes.string.isRequired,
-            name: PropTypes.string.isRequired,
-            isOnline: PropTypes.bool.isRequired,
-            id: PropTypes.number.isRequired,
-        })
-    ),
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ),
 };
 
 export { FriendList };
--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,23 +1,23 @@
 import { FriendItem, FriendName, FriendStatus } from './FriendListItem.styled';
 import PropTypes from 'prop-types';
 
-function FriendListItem({ avatar, name, isOnline, id }) {
-    return (
-        <FriendItem key={id}>
-            <FriendStatus status={isOnline} />
-            <img className="avatar" src={avatar} alt="User avatar" width="48" />
-            <FriendName>{name}</FriendName>
-        </FriendItem>
-    );
+function FriendListItem({ avatar, name, isOnline }) {
+  return (
+    <FriendItem>
+      <FriendStatus status={isOnline} />
+      <img className="avatar" src={avatar} alt="User avatar" width="48" />
+      <FriendName>{name}</FriendName>
+    </FriendItem>
+  );
 }
 
 FriendItem.propTypes = {
-    friend: PropTypes.shape({
-        avatar: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        isOnline: PropTypes.bool.isRequired,
-        id: PropTypes.number.isRequired,
-    }),
+  friend: PropTypes.shape({
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    isOnline: PropTypes.bool.isRequired,
+    id: PropTypes.number.isRequired,
+  }),
 };
 
 export { FriendListItem };
